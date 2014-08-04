99e1bf3d0d2a8fad64f31e41d75bfe57714cb309
KareMatKare <rrtt96@naver.com>
2014-08-05 08:22:24 +0900

    back
    back

---
+++ Coupon/gen/net/tourcoupon/coupon/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.tourcoupon.coupon;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Coupon/src/net/tourcoupon/coupon/MainActivity.java
@@ -0,0 +1,28 @@
+package net.tourcoupon.coupon;
+
+import org.mospi.moml.framework.pub.core.MOMLActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends MOMLActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        
+//        boolean test=getMomlView().registerUIComponent("org.mospi.moml.component.qrcode.componentUIQrcode", "QRCODE", "WINDOW", null);
+//        System.out.println(test);
+        
+	    this.loadApplication("applicationInfo.xml");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
+
